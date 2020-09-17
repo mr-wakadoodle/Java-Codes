ae17d10e9ac7d5d184f26df9ebf1e32ed2a94ec4
mr-wakadoodle <55294664+mr-wakadoodle@users.noreply.github.com>
2020-09-17 15:09:17 +0530

    Update MiddleSumMerge.java

--- MiddleSumMerge.java
+++ MiddleSumMerge.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class MiddleSumMerge {
-	// https://www.geeksforgeeks.org/median-of-two-sorted-arrays/
+	
 	public static int getMedian(int a[], int b[], int StartA, int StartB, int EndA, int EndB) {
 		if (EndA-StartA == 1) {
 			return (Math.max(a[StartA], b[StartB]) + Math.min(a[EndA], b[EndB]))/2;
@@ -27,39 +27,7 @@ public class MiddleSumMerge {
 		}
 	}
 	
-	/*
-	 *My Approach O(n)
-	 * public static int MergeSumMiddle(int a[], int b[]) {
-		int n=a.length;
-		int temp1[]=new int[n+1];
-		int temp2[]=new int[n+1];
-		int farray[]=new int[2*n];
-		int i;
-		temp1[temp1.length-1]=99999;
-		temp2[temp2.length-1]=99999;
-		int k=0;
-		for (i=0;i<temp1.length-1;i++) {
-			temp1[i]=a[k];
-			temp2[i]=b[k];
-			k++;
-		}
-		int t1=0,t2=0;
-		for (i=0;i<farray.length;i++) {
-			if (temp1[t1]<temp2[t2]) {
-				farray[i]=temp1[t1];
-				if (t1<temp1.length) {t1++;}
-			}
-			else {
-				farray[i]=temp2[t2];
-				if (t2<temp2.length) {t2++;}
-			}
-		}
-		int l=farray.length;
-		System.out.println(Arrays.toString(farray));
-		int sum=0;
-		sum=farray[l/2-1]+farray[l/2];
-		return sum;
-	}*/
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -77,8 +45,7 @@ public class MiddleSumMerge {
 			for (i=0;i<n;i++) {
 				b[i]=ob.nextInt();
 			}
-			//sum=MergeSumMiddle(a, b); //My Approach O(n)
-			//Geeks for Geeks O(log n)
+			
 			sum=getMedian(a, b, a[0], b[0], a[n-1], b[n-1]);
 			System.out.println(sum);
 			t--;
